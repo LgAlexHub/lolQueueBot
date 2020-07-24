8bb5ef6f3a1b1a6e81d911e7ee115354b0318eb5
AlexLg <alexlegras@hotmail.com>
2020-07-24 19:39:38 +0200

    ANTONIN

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 import * as Discord from 'discord.js';
 const client = new Discord.Client();
 import {
-  knowMyRankByName
+  knowMyRankByName, specAGame, getChamionList
 } from './func.js';
 import config from './config.js';
 
@@ -11,14 +11,22 @@ client.on('ready', () => {
 
 client.on('message', async msg => {
   const stringMsg = msg.content;
+  if (stringMsg === "i") {
+    console.log(await getChamionList());
+  }
   if (stringMsg.match(/^-lolRank (.*)/) != null) {
     const summonerName = (stringMsg.match(/^-lolRank (.*)/)[1]);
     const res = await knowMyRankByName(summonerName);
     if (res.length > 1) {
       msg.reply(res);
     } else {
-      msg.reply("Ce joueur n'a jamais fait de Ranked ou n'existe pas :( ");
+      msg.reply("Ce joueur n'a jamais fait de Ranked");
     }
+  }
+  if (stringMsg.match(/^-spec (.*)/) != null) {
+    const summonerName = (stringMsg.match(/^-spec (.*)/)[1]);
+    const res = await specAGame(summonerName);
+    msg.reply(res);
   }
   if (msg.content.match("ping")) {
     msg.channel.send("test");
--- func.js
+++ func.js
@@ -2,28 +2,53 @@ import fetch from 'node-fetch';
 import config from './config.js';
 
 function fetchApi(stringUrl) {
-    return fetch(stringUrl + `?api_key=${config.riot.apiKey}`).then(res => res.json());
+    return fetch(encodeURI(stringUrl + `?api_key=${config.riot.apiKey}`)).then(res => res.json());
+}
+
+async function getChamionList() {
+    let version =await fetch(encodeURI("https://ddragon.leagueoflegends.com/api/versions.json")).then(res => res.json());
+    let championList=fetch(encodeURI(`http://ddragon.leagueoflegends.com/cdn/${version[0]}/data/en_US/champion.json`)).then(res=>res.json());
+    return championList;
+}
+
+async function specAGame(summonerName) {
+    const summonerObj = await getSummonerObjByName(summonerName);
+    const summonerId = summonerObj.id;
+    const gameObj = await fetchApi("https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/" + summonerId);
+    if (gameObj.gameId == undefined) return "Pas de partie en cours pour ce joueur";
+    let resString ="";
+    const participantsArrayObj = gameObj.participants;
+    for (let i = 0 ; i < participantsArrayObj.length ; i++){
+        resString += await knowMyRankByName(participantsArrayObj[i].summonerName);
+        if(participantsArrayObj[i].teamId == 100) {
+            resString +=":blue_circle:"+"\n";
+        }else{
+            resString +=":red_circle:"+"\n";
+        }
+    }
+    return resString;
+}
+
+async function getSummonerObjByName(summonerName) {
+    return fetchApi("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName);
 }
 
 async function knowMyRankByName(summonerName) {
-    const summonerObj = await fetchApi("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName);
+    const summonerObj = await getSummonerObjByName(summonerName);
     const summonerId = summonerObj.id;
     const objRanked = await fetchApi("https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + summonerId);
-
     if (!objRanked.length > 0) return "";
-
     let returnString = "";
-
     for (let i = 0; i < objRanked.length; i++) {
+        returnString += "Nom d'invocateur: **" + objRanked[i].summonerName + "**\n";
         if (objRanked[i].queueType == "RANKED_SOLO_5x5") {
-            returnString += "Nom d'invocateur: " + objRanked[i].summonerName + "\n";
-            returnString += "Rang: " + objRanked[i].tier + " " + objRanked[i].rank + " " + objRanked[i].leaguePoints + " lp\n";
-            returnString += "Win: " + objRanked[i].wins + " Losses: " + objRanked[i].losses + " WinRate: " + (objRanked[i].wins / (objRanked[i].wins + objRanked[i].losses) * 100).toFixed(2) + "%";
+            returnString += "Rang: **" + objRanked[i].tier + " " + objRanked[i].rank + " " + objRanked[i].leaguePoints + " **lp\n";
+            returnString += "Win: **" + objRanked[i].wins + " **Losses:** " + objRanked[i].losses + " **WinRate:** " + (objRanked[i].wins / (objRanked[i].wins + objRanked[i].losses) * 100).toFixed(2) + "**%\n";
         }
     }
     return returnString;
 }
 
 export {
-    knowMyRankByName
+    knowMyRankByName, specAGame, getChamionList
 }
